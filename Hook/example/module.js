e2fc86fe355c22624641f91212382e841086545b
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-08-11 13:25:59 +0900

    Update module.js

--- Hook/example/module.js
+++ Hook/example/module.js
@@ -1,1 +1,18 @@
-
+import React from 'react';
+import { useState } from 'react';
+function App() {
+  const [count, setCoust] = useState(0);
+  return(
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={()=>setCoust(count + 1)}>
+        click me
+      </button>
+     </div>
+  );
+}
+export default App;
+/*この例のuseStateがフックです。関数のコンポーネントの中でローカルなstateを使うために呼び出しています。
+このstateは以降の再レンダーの間にもReactによって保持されます。
+useStateは現在のstateの値と、それを更新するための関数とをペアにして返す
+*/
