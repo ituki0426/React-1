de52286cbae05ebda4c6f6bde7aad1e955a7d312
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-08-15 12:51:04 +0900

    Update App.js

--- props/使い方まとめ/親から子への値渡し/App.js
+++ props/使い方まとめ/親から子への値渡し/App.js
@@ -1,1 +1,12 @@
-
+import React from "react";
+function App(){
+  return(
+    <MyComponent title="react" />
+  )
+}
+function MyComponent(props){
+  return(
+    <h1>Hello {props.title}!</h1>
+  )
+}
+export default App;
