719bfe15d5cacac06a661f4ba59da4850d7d0ff9
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-08-17 17:57:44 +0900

    Update index.js

--- for/index.js
+++ for/index.js
@@ -1,1 +1,17 @@
+ var list = [];
 
+  var data = [
+    { text: "1" },
+    { text: "2" }
+  ];
+
+  for(var i in data){
+    list.push(<li>{data[i].text}</li>);
+  }
+
+  return(
+    <ul>
+      {list}
+    </ul>
+  );
+}
