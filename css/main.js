7a55261d22e10dac3abc0cde4510845a02e15318
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-09-02 16:19:21 +0900

    Update main.js

--- css/main.js
+++ css/main.js
@@ -1,1 +1,44 @@
+// 謎の定義
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const mixStyles = function(){
+  var res = {};
+  for (var i = 0; i < arguments.length; ++i) {
+    if (arguments[i]) Object.assign(res, arguments[i]);
+  }
+  return res;
+}
+
+// スタイルを定義
+const Styles = {
+  bgRed: {
+    backgroundColor: "red",
+  },
+  bgBlue: {
+    backgroundColor: "blue",
+  },
+  fontSize16: {
+    fontSize: "16px",
+  },
+  fontSize20: {
+    fontSize: "20px",
+  }
+}
+
+// スタイル適用
+class StyleComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <p style={mixStyles(Styles.bgRed, Styles.fontSize16)}>背景：赤,文字サイズ：16</p>
+        <p style={mixStyles(Styles.bgRed, Styles.fontSize20)}>背景：赤,文字サイズ：20</p>
+      </div>
+    )
+  }
+}
+
+export default ListComponent;
+
 
